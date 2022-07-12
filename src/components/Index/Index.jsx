483a3ffbe8261d5e7b54d84fe8ab5f3084f96e86
starwalker <juan.rodriguez2803@hotmail.com>
2022-07-11 21:21:08 -0500

    name changed scss doc popular cat added

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,64 +1,14 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGuitar } from '@fortawesome/free-solid-svg-icons';
-import { faTruck } from '@fortawesome/free-solid-svg-icons';
-import { faHeadset } from '@fortawesome/free-solid-svg-icons';
-import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
 import Slider from '../Slider/Slider';
+import Ads from '../Index/Ads/Ads';
+import PopItems from '../Index/Popular-Items/PopItems';
 
 const Index = () => {
-
-    const guitar = <FontAwesomeIcon icon={faGuitar} size="3x" />
-    const truck = <FontAwesomeIcon icon={faTruck} size="2x" />
-    const headset = <FontAwesomeIcon icon={faHeadset} size="2x" />
-    const creditCard = <FontAwesomeIcon icon={faCreditCard} size="2x" />
-
     return (
         <div className='container'>
             <Slider />
-            <section className='value-ads'>
-                <h2>Get More at Wave Store</h2>
-                <div className='value-ads__links'>
-                    <a href="">
-                        <div className="value-ads__icon">
-                            <span>{guitar}</span>
-                        </div>
-                        <div className="value-ads__title">Guitar Inspection</div>
-                        <div className="value-ads__subtitle">
-                            Guitar perfection
-                            <span>right out of the box.</span>
-                        </div>
-                        <div className="value-ads__cta">Learn More</div>
-                    </a>
-                    <a href="">
-                        <div className="value-ads__icon">
-                            <span>{truck}</span>
-                        </div>
-                        <div className="value-ads__title">Fast, FREE Shipping</div>
-                        <div className="value-ads__subtitle">Even on the small stuff.</div>
-                        <div className="value-ads__cta">Learn More</div>
-                    </a>
-                    <a href="">
-                        <div className="value-ads__icon">
-                            <span>{headset}</span>
-                        </div>
-                        <div className="value-ads__title">FREE Tech Support</div>
-                        <div className="value-ads__subtitle">
-                            Got a question?
-                            <span>We're here to help</span>
-                        </div>
-                        <div className="value-ads__cta">Learn More</div>
-                    </a>
-                    <a href="">
-                        <div className="value-ads__icon">
-                            <span>{creditCard}</span>
-                        </div>
-                        <div className="value-ads__title">Easy Payments</div>
-                        <div className="value-ads__subtitle">Up to 48 Months.</div>
-                        <div className="value-ads__cta">Learn More</div>
-                    </a>
-                </div>
-            </section>
+            <Ads />
+            <PopItems />
         </div>
     )
 }
