94e38cdd3ad01482f09a737f75440f81dadd10a7
starwalker <juan.rodriguez2803@hotmail.com>
2022-09-05 19:52:18 -0500

    Payment details design added

--- src/components/MyAccount/UserAccount/Payment-options/PaymentOptions.jsx
+++ src/components/MyAccount/UserAccount/Payment-options/PaymentOptions.jsx
@@ -1,54 +1,106 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCcVisa } from '@fortawesome/free-brands-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 const PaymentOptions = () => {
   const visa = <FontAwesomeIcon icon={faCcVisa} />
-  return (
-    <div className='container'>
-      <h3>Payment Information</h3>
-      <hr />
-      <div className="d-flex justify-content-center pb-3">
-        <div className="col-2 payment-box">
-          <div className="payment-info" id='card-info-title'>
-            <p>My Card</p>
+  const home = <FontAwesomeIcon icon={faHome} />
+  const CardInfo = () => {
+    return (
+      <div className='col card-info-border'>
+        <div className="d-flex justify-content-center pb-2">
+          <div className="col-8 payment-box" id='payment-info-box'>
+            <div className="payment-info">
+              <label>Name on Card</label>
+              <input type="text" className='form-control' placeholder='Juan Alberto Rodriguez Sotelo' disabled />
+            </div>
+            <div className="payment-info">
+              <label>Card Number</label>
+              <div className="input-group" id='card-icon-input-group'>
+                <input type="text" className='form-control' placeholder='1111 2222 3333 4444' disabled />
+                <span className='input-group-text'>{visa}</span>
+              </div>
+            </div>
+          </div>
+          <div className="col" id='payment-info-box'>
+            <div className="payment-info">
+              <div className="col">
+                <label>Expiration Date</label>
+                <input type="text" className='form-control' placeholder='MM/YYYY' disabled />
+              </div>
+            </div>
+            <div className="payment-info">
+              <div className="col">
+                <label>CVV</label>
+                <input type="password" className='form-control' value={'123'} disabled />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-8 payment-box" id='payment-info-box'>
-          <div className="payment-info">
-            <label>Name on Card</label>
-            <input type="text" className='form-control' placeholder='Juan Alberto Rodriguez Sotelo' disabled />
+        <div className="col border-button-edit-card pt-3">
+          <div className="row justify-content-start">
+            <div className="col-2"><button className="card-button__edit">Edit Card</button></div>
+            <div className="col-2"><button className="card-button__cancel">Cancel</button></div>
           </div>
-          <div className="payment-info">
-            <label>Card Number</label>
+        </div>
+      </div>
+    )
+  }
+
+  const MyCard = () => {
+    return (
+      <div className='col-12 card-info-border'>
+        <div class="row payment-info">
+          <div className="col-sm-9" id='payment-info-box'>
             <div className="input-group" id='card-icon-input-group'>
+              <label className='col-sm-3 col-form-label'>My card</label>
               <input type="text" className='form-control' placeholder='1111 2222 3333 4444' disabled />
               <span className='input-group-text'>{visa}</span>
             </div>
           </div>
-        </div>
-        <div className="col" id='payment-info-box'>
-          <div className="payment-info">
-            <div className="col">
-              <label>Expiration Date</label>
-              <input type="text" className='form-control' placeholder='MM/YYYY' disabled />
-            </div>
-          </div>
-          <div className="payment-info">
-            <div className="col">
-              <label>CVV</label>
-              <input type="password" className='form-control' value={'123'} disabled />
-            </div>
+          <div className="col">
+            <button className="card-button__edit">See Details</button>
           </div>
         </div>
       </div>
-      <div className="d-flex justify-content-center">
-        <div className="col-2"></div>
-        <div className="col border-buttton-edit-card pt-4 pb-2">
-          <button className="edit-card-button">Edit Card</button>
+    )
+  }
+
+  const MyAddress = () => {
+    return (
+      <div className='col-12 card-info-border'>
+        <div class="row payment-info">
+          <div className="col-sm-9" id='payment-info-box'>
+            <div className="input-group" id='card-icon-input-group'>
+              <label className='col-sm-3 col-form-label'>My Addresss</label>
+              <input type="text" className='form-control' placeholder='795 Folsom Ave, Suite 600' disabled />
+              <span className='input-group-text'>{home}</span>
+            </div>
+          </div>
+          <div className="col">
+            <button className="card-button__edit">See Details</button>
+          </div>
         </div>
       </div>
-      <hr />
+    )
+  }
+
+  return (
+    <div className='container'>
+      <div className="pt-2">
+        <h5>Billing Information</h5>
+      </div>
+      <div className="pb-3">
+        {true ? (<MyCard />) : (<CardInfo />)}
+      </div>
+      <br />
+      <div className="pb-2 pt-4">
+        <h5>Shipping Information</h5>
+        <div className="pb-3">
+          <MyAddress />
+        </div>
+      </div>
     </div>
   )
 }
