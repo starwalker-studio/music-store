6d029416af4dce4976cce624eba6802df27c53a4
starwalker <juan.rodriguez2803@hotmail.com>
2023-05-25 16:21:25 -0600

    shell file command correction

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,11 +10,11 @@ if [[ $1 = "prod" ]] && [[ $2 = "down" || $2 = "up" ]]; then
     echo "╚███╔███╔╝██║  ██║ ╚████╔╝ ███████╗    ███████║   ██║   ╚██████╔╝██║  ██║███████╗";
     echo " ╚══╝╚══╝ ╚═╝  ╚═╝  ╚═══╝  ╚══════╝    ╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝";
     echo "                                                                                 ";
-    echo "\n"
+    echo ""
     echo "Running docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp"
-    echo "\n"
+    echo ""
     echo "WAVE STORE PRODUCTION running on nginx server ports:8080"
-    docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp
+    docker compose -f docker-compose.yaml -f $fileEnv $downOrUp
 else
     echo 'Need to follow format ./deploy prod|dev down|up'
 fi
