1b1226b1072c0313cde98459c9e413c61a4c592f
starwalker <juan.rodriguez2803@hotmail.com>
2023-05-25 15:48:40 -0600

    docker files added for production deployment

---
+++ bin/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ $1 = "prod" ]] && [[ $2 = "down" || $2 = "up" ]]; then
+    cd ..
+    fileEnv="docker-compose.${1}.yaml"
+    downOrUp=$2
+    echo "██╗    ██╗ █████╗ ██╗   ██╗███████╗    ███████╗████████╗ ██████╗ ██████╗ ███████╗";
+    echo "██║    ██║██╔══██╗██║   ██║██╔════╝    ██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝";
+    echo "██║ █╗ ██║███████║██║   ██║█████╗      ███████╗   ██║   ██║   ██║██████╔╝█████╗  ";
+    echo "██║███╗██║██╔══██║╚██╗ ██╔╝██╔══╝      ╚════██║   ██║   ██║   ██║██╔══██╗██╔══╝  ";
+    echo "╚███╔███╔╝██║  ██║ ╚████╔╝ ███████╗    ███████║   ██║   ╚██████╔╝██║  ██║███████╗";
+    echo " ╚══╝╚══╝ ╚═╝  ╚═╝  ╚═══╝  ╚══════╝    ╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝";
+    echo "                                                                                 ";
+    echo "\n"
+    echo "Running docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp"
+    echo "\n"
+    echo "WAVE STORE PRODUCTION running on nginx server ports:8080"
+    docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp
+else
+    echo 'Need to follow format ./deploy prod|dev down|up'
+fi
